7134efa1a8ab39412a472f64df00bbf118375171
Eder <ederfmartins@gmail.com>
2026-07-30 18:46:08 -0300

    Rebalance moves

--- src/data/pokemon/tmhm_learnsets.h
+++ src/data/pokemon/tmhm_learnsets.h
@@ -506,6 +506,7 @@ static const u32 sTMHMLearnsets[][2] =
                                         | TMHM(TM44_REST)
                                         | TMHM(TM45_ATTRACT)
                                         | TMHM(TM46_THIEF)
+                                        | TMHM(HM03_SURF)
                                         | TMHM(HM04_STRENGTH)
                                         | TMHM(HM05_FLASH)
                                         | TMHM(HM06_ROCK_SMASH)),
